86ea20bd6fc2140245e678a36e54cdd904e1342b
joeryoost <joeryoost@gmail.com>
2019-11-06 11:38:45 +0100

    Added navigation functions to vehiclemenu

--- app/src/main/java/com/example/univefinal/VehicleMenu.kt
+++ app/src/main/java/com/example/univefinal/VehicleMenu.kt
@@ -1,10 +1,16 @@
 package com.example.univefinal
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_vehicle_menu.*
+import kotlinx.android.synthetic.main.activity_vehicle_menu.toolbar
 
 class VehicleMenu : AppCompatActivity() {
 
@@ -13,6 +19,38 @@ class VehicleMenu : AppCompatActivity() {
         setContentView(R.layout.activity_vehicle_menu)
         setSupportActionBar(toolbar)
 
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = ""
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        toolbar.setTitleTextColor(Color.BLACK)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.to_main_menu) {
+            Toast.makeText(this, "To Main menu", Toast.LENGTH_LONG).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
 }
