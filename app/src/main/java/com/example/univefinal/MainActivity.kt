bd7ad5d0ded080f7793e0b002e83d3e153319f6c
Mick Nieweg (student) <s1123787@student.windesheim.nl>
2019-12-11 13:00:26 +0100

    Fixed youris dinguh

--- app/src/main/java/com/example/univefinal/MainActivity.kt
+++ app/src/main/java/com/example/univefinal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.univefinal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.univefinal.AppMethods.Companion.isOnline
 
 class MainActivity : AppCompatActivity() {
@@ -19,29 +20,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
-
-        //actionbar
-//        val actionbar = supportActionBar
-        //set actionbar title
-//        actionbar!!.title = ""
-        //set back button
-//        actionbar.setDisplayHomeAsUpEnabled(true)
-//        actionbar.setDisplayHomeAsUpEnabled(true)
-//        toolbar.setTitleTextColor(Color.BLACK)
-
-//        webView.loadUrl("https://www.unive.nl/hypotheek/")
-//        webView.loadUrl("https://www.unive.nl/schadeservice/")
-//        webView.loadUrl("https://www.unive.nl/autoverzekering/tips-tweedehands-auto-kopen")
-//        webView.loadUrl("https://www.unive.nl/zorgverzekering/zorgcheck/")
-
-        //privatelease??
-
-//        intent.putExtra("webviewTitle", "Bereken uw hypotheek")
-//        intent.putExtra("webviewTitle", "Zorgverzekering")
-//        intent.putExtra("webviewTitle", "Schadehersteller")
-//        intent.putExtra("webviewTitle", "Zorgverzekering")
-/*
+        if (supportActionBar != null)
+            supportActionBar?.hide()
         //button actions
         val buyCar = findViewById<Button>(R.id.buyCar)
         buyCar.setOnClickListener{
@@ -71,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("webviewTitle", "Zorgverzekering")
             intent.putExtra("webviewUrl", "https://www.unive.nl/zorgverzekering/zorgcheck/")
             startActivity(intent)
-        }*/
+        }
     }
 
 }
